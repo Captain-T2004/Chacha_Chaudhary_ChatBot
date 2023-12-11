139d19d5fc92e941556fc133a210b3f7960e02e0
Captain-T2004 <126911424+Captain-T2004@users.noreply.github.com>
2023-12-11 16:53:50 +0530

    This is working and the output of llm is also shown on the site.
    Now we need to focus on adding text to speech on the website

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-import engine.stt as stt
+from flask import Flask, render_template, request, jsonify
 import engine.llm as llm
 import engine.tts as tts
 
@@ -10,22 +9,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/process_audio', methods=['POST'])
+@app.route('/', methods=['POST'])
 def process_audio():
     try:
-        question = stt.speech_to_text()
+        question = request.get_json()['question']
         response = llm.llm(question=question)
-
-        # Text response
-        text_response = f"Question: {question}\nResponse: {response}"
-
-        # Audio response
-        tts.text_to_speech(text_response)
-
-        return render_template('index.html', question=question, response=response, text_response=text_response)
+        text_response = f"Response: {response}"
+        print(text_response)
+        return jsonify(result=text_response)
     
     except RuntimeError:
-        # Restart the speech recognition loop
         return render_template('index.html', error="RuntimeError occurred. Please try again.")
 
 if __name__ == '__main__':
